556c74b1bc35e1698da238274eff9e9fd3d9f0e9
tony612 <h.bing612@gmail.com>
2013-02-26 17:02:15 +0800

    add main key to refinery_videos_video_pages

--- db/migrate/3_create_page_videos.rb
+++ db/migrate/3_create_page_videos.rb
@@ -1,6 +1,6 @@
 class CreatePageVideos < ActiveRecord::Migration
   def change
-    create_table Refinery::Videos::VideoPage.table_name, :id => false do |t|
+    create_table Refinery::Videos::VideoPage.table_name do |t|
       t.integer :video_id
       t.integer :page_id
       t.integer :position
